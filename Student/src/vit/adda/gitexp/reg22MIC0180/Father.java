ae3811b6a00d49b0136c3c67f52e1bc83a39355d
Raakesh <raakesh.mj7@gmail.com>
2024-08-05 20:16:40 +0530

    Initial Commit

---
+++ Student/src/vit/adda/gitexp/reg22MIC0180/Father.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vit.adda.gitexp.reg22MIC0180;
+
+/**
+ *
+ * @author Raakesh MJ
+ */
+public class Father extends Family {
+    
+    String fullName;
+    String birthDate;
+}
---
+++ Student/src/vit/adda/gitexp/reg22MIC0180/Student.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vit.adda.gitexp.reg22MIC0180;
+
+/**
+ *
+ * @author Raakesh MJ
+ */
+public class Student {
+    int studentID;
+    String fullName ;
+    String address;
+    int schoolYear;
+    
+    public static void main(String[] args) {
+            
+            }
+}
+
